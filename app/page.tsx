e7081108072652848bab256315661d9b73fd2542
Zia Beg <s235124@dtu.dk>
2025-05-12 17:08:32 +0200

    Login page updated with contact information

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,7 @@ console.log("v"+env.NEXT_PUBLIC_API_BASE_URL)
         <div className="w-full max-w-sm">
           <h3 className='text-white text-sm mb-4'>A DIGITAL SOLUTION FOR YOUR SCHOOL</h3>
           <h2 className="text-3xl font-bold mb-6 text-left text-white">Log in to your account<span className='text-blue-500'>.</span></h2>
-          <h3 className='text-white text-sm my-4'>Mangler du en konto? <Link href="/signup" className='text-blue-500'>Lav en her</Link></h3>
+          <h3 className='text-white text-sm my-4'>Interested in this service? <Link href="mailto:s235124@dtu.dk?subject=Applying for Learnflow&body=Hi, my name is (First name) (Last name), I would like to use LearnFlow for my shcool (School name). Optionally, my phone number is (Phone number)" className='text-blue-500'>Contact us.</Link></h3>
           <form className="space-y-6" onSubmit={login3}>
             <FloatingLabelInput value={email} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} label="Email" id="email"/>
             <FloatingLabelInput value={password} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} label="Password" id="password"/>
